9290f2a28144ae5ffbf7f54aa8019dbeb0d11465
thaty23 <138634241+thaty23@users.noreply.github.com>
2023-07-04 22:24:04 -0300

    Add files via upload
    aqui contém as respostas das atividades da turma 1, data: 27/06/2023, Aluna: Tatiane Clemente Da Silva

---
+++ .at2.js
@@ -0,0 +1,19 @@
+// Variáveis usando var
+var x = 5;
+var y = 7;
+
+// Variáveis usando let
+let a = 10;
+let b = 20;
+
+// Constantes usando const
+const PI = 3.14;
+const MAX_VALUE = 100;
+
+console.log(x);
+console.log(y);
+console.log(a);
+console.log(b);
+console.log(PI);
+console.log(MAX_VALUE);
+
